120f300d99782c7c268586764e5a590c5f749543
RituRajPS <107362757+riturajps-india@users.noreply.github.com>
2022-08-12 21:57:03 +0530

    Update chat.js

--- SimpleChat/Private-Chat/chat.js
+++ SimpleChat/Private-Chat/chat.js
@@ -1,13 +1,13 @@
 // Your web app's Firebase configuration
 var config = {
-  apiKey: "AIzaSyAS7jpXkSkQimFVxqh4KB9JJWAVwYeLQMc",
-  authDomain: "riturajps-official.firebaseapp.com",
-  databaseURL: "https://riturajps-official-default-rtdb.firebaseio.com",
-  projectId: "riturajps-official",
-  storageBucket: "riturajps-official.appspot.com",
-  messagingSenderId: "290350549856",
-  appId: "1:290350549856:web:ffa18e4491fdc366536c05",
-  measurementId: "G-B5PVWMJSYG"
+  apiKey: "AIzaSyBhXL42W9FIlNGEQjJl_pdEA3At_2d0-AQ",
+  authDomain: "primarchweb-app.firebaseapp.com",
+  databaseURL: "https://primarchweb-app-default-rtdb.firebaseio.com",
+  projectId: "primarchweb-app",
+  storageBucket: "primarchweb-app.appspot.com",
+  messagingSenderId: "935109999830",
+  appId: "1:935109999830:web:68ad1f1e6e8d4fa9035d77",
+  measurementId: "G-S1RXNX9F55"
 };
 // Initialize Firebase
 firebase.initializeApp(config);
